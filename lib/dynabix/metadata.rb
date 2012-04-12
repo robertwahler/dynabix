5d06b98b7b19b8b812d6a8646370fb8843b8cad7
Robert Wahler <robert@gearheadforhire.com>
2012-04-12 14:59:18 -0400

    spec and code for has_metadata

--- lib/dynabix/metadata.rb
+++ lib/dynabix/metadata.rb
@@ -1,9 +1,41 @@
 require 'active_record'
 
 module Dynabix
-  module Metatdata
+  module Metadata
+
+    def has_metadata(serializer=:metadata, *attributes)
+      serialize(serializer, Hash)
+
+      # Requires Ruby 1.9+
+      #
+      # define the class accessor for setting attributes
+      define_singleton_method "#{serializer}_accessor" do |*attrs|
+        attrs.each do |attr|
+          create_accessor(serializer, attr)
+        end
+      end
+
+      # define each of the attributes for this serializer
+      attributes.each do |attr|
+        create_accessor(serializer, attr)
+      end
+    end
+
+    private
+
+    def create_accessor(serializer, attr)
+
+      define_method("#{attr}") do
+        self[serializer][attr]
+      end
+
+      define_method("#{attr}=") do |value|
+        self[serializer][attr] = value
+      end
+
+    end
 
   end
 end
 
-ActiveRecord::Base.extend Dynabix::Metatdata
+ActiveRecord::Base.extend Dynabix::Metadata
--- spec/dynabix/metadata_spec.rb
+++ spec/dynabix/metadata_spec.rb
@@ -1,5 +1,49 @@
 require 'spec_helper'
 
-describe Dynabix::Metatdata do
+class Thing < ActiveRecord::Base
+  has_metadata
+
+  metadata_accessor :rv_doors, :rv_windows
+end
+
+describe Dynabix::Metadata do
+
+  describe "has_metadata" do
+
+    it "should be a class method" do
+      defined?(Thing.has_metadata).should be_true
+      defined?(Thing.create_accessor).should be_false
+    end
+
+  end
+
+  describe "creating a serializer" do
+
+    context "has_metadata with no params" do
+
+      it "should create a default instance serializer accessor called 'metadata'" do
+        defined?(Thing.metadata).should be_false
+        thing = Thing.new
+        thing.should be_valid
+        defined?(thing.metadata).should be_true
+        thing.metadata.should == {}
+      end
+
+      it "should create a default class level accessor called 'metadata_accessor'" do
+        defined?(Thing.metadata_accessor).should be_true
+        defined?(Thing.rv_windows).should be_false
+
+        thing = Thing.new
+        defined?(thing.rv_windows).should be_true
+        thing.metadata[:rv_windows].should be_nil
+        thing.metadata[:rv_windows] = 1
+        thing.metadata[:rv_windows].should == 1
+        thing.rv_windows = 2
+        thing.rv_windows.should == 2
+      end
+
+    end
+
+  end
 
 end
